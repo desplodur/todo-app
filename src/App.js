5c01a41849a97ccfcf7ca4247fef52e601c2d5c2
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-05-31 11:10:31 +0200

    jsx done

--- src/App.js
+++ src/App.js
@@ -9,37 +9,84 @@ function App() {
       id: nanoid(),
       task: "Do React Exercise",
       status: false,
+      archived: false,
     },
     {
       id: nanoid(),
       task: "Feed the dog",
       status: true,
+      archived: false,
     },
     {
       id: nanoid(),
       task: "Go running",
       status: false,
+      archived: false,
     },
     {
+      id: nanoid(),
       task: "Cook Dinner",
       status: true,
+      archived: false,
     },
   ]);
+
+  const createNewTodo = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      task: event.target.taskText.value,
+      status: false,
+      archived: false,
+    };
+    const newToDoList = [...toDoList, newTodo];
+    setToDo(newToDoList);
+  };
+
+  function deleteToDo(id) {
+    const myIndex = toDoList.findIndex((todo) => {
+      return todo.id === id;
+    });
+    const newToDoList = [...toDoList];
+    newToDoList.splice(myIndex, 1);
+    setToDo(newToDoList);
+  }
+
+  function changeStatus(id) {
+    const myIndex = toDoList.findIndex((todo) => {
+      return todo.id === id;
+    });
+    const newToDoList = [...toDoList];
+    newToDoList[myIndex].status = !toDoList[myIndex].status;
+    setToDo(newToDoList);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>ToDo List</h1>
-        <form>
+        <form onSubmit={createNewTodo}>
           <label>new ToDo:</label>
-          <input type="text"></input>
+          <input name="taskText" type="text"></input>
           <button type="submit">Create a new Task</button>
         </form>
       </header>
       <main>
         {toDoList.map((todo) => {
           return (
-            <Todo key={todo.id} taskProp={todo.task} statusProp={todo.status} />
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              taskProp={todo.task}
+              statusProp={todo.status}
+              archivedProp={todo.archived}
+              changeStatus={() => {
+                changeStatus(todo.id);
+              }}
+              deleteToDo={() => {
+                deleteToDo(todo.id);
+              }}
+            />
           );
         })}
       </main>
